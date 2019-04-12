168097a0c4569803bd32a7dfc5368dd656fd931e
marween <annemagali.saintgeorges@gmail.com>
2019-04-12 16:30:39 +0200

    scheduler

--- app/Console/Commands/SchedulerDaemon.php
+++ app/Console/Commands/SchedulerDaemon.php
@@ -1,9 +1,8 @@
 <?php
-
 namespace App\Console\Commands;
-
+use Carbon\Carbon;
 use Illuminate\Console\Command;
-
+use Illuminate\Support\Facades\Artisan;
 class SchedulerDaemon extends Command
 {
     /**
@@ -11,15 +10,13 @@ class SchedulerDaemon extends Command
      *
      * @var string
      */
-    protected $signature = 'command:name';
-
+    protected $signature = 'schedule:daemon';
     /**
      * The console command description.
      *
      * @var string
      */
     protected $description = 'Command description';
-
     /**
      * Create a new command instance.
      *
@@ -29,13 +26,12 @@ class SchedulerDaemon extends Command
     {
         parent::__construct();
     }
-
     /**
      * Execute the console command.
      *
      * @return mixed
      */
-     public function handle()
+    public function handle()
     {
         \Log::info('In scheduler daemon');
         while (true) {
