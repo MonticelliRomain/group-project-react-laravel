db0a1cc8e624d108414876ccdc59350158c18549
marween <annemagali.saintgeorges@gmail.com>
2019-04-12 11:12:21 +0200

    scheduler

--- database/factories/EventFactory.php
+++ database/factories/EventFactory.php
@@ -8,6 +8,7 @@ $factory->define(App\Event::class, function (Faker $faker) {
         'date_event' => $faker->dateTime(),
         'description' => $faker->text(),
         'reminder' => $faker->dateTime(),
-        'author' =>factory ('App\User')->create()->id
+        'author' =>factory ('App\User')->create()->id,
+        'reminded'=>false
     ];
 });
