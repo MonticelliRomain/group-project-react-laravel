baae5434b60436f09c00a7bb6efd6e73172224f6
marween <annemagali.saintgeorges@gmail.com>
2019-04-15 11:37:29 +0200

    test heroku refresh table

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -31,24 +31,24 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
 
-        \Log::info('In Kernel schedule function');
-        $reminder = DB::table('events')
-                        ->join('list_of_participants', 'list_of_participants.event', 'events.id')
-                        ->join('users' , 'list_of_participants.participant', '=','users.id' )
+        // \Log::info('In Kernel schedule function');
+        // $reminder = DB::table('events')
+        //                 ->join('list_of_participants', 'list_of_participants.event', 'events.id')
+        //                 ->join('users' , 'list_of_participants.participant', '=','users.id' )
                         
-                        ->select('users.email AS email', 'users.name AS name', 'events.name AS eventName','events.date_event', 'events.id')
-                        ->where('events.reminder', '<=', 'NOW()')
-                        ->where('list_of_participants.reminded', 'false')
-                        ->get();
+        //                 ->select('users.email AS email', 'users.name AS name', 'events.name AS eventName','events.date_event', 'events.id')
+        //                 ->where('events.reminder', '<=', 'NOW()')
+        //                 ->where('list_of_participants.reminded', 'false')
+        //                 ->get();
                     
-                    DB::table('list_of_participants')
-                        ->join('events', 'list_of_participants.event', 'events.id')
-                        ->where('events.reminder', '<=', 'NOW()')
-                        ->update(['reminded' => 'true']);
+        //             DB::table('list_of_participants')
+        //                 ->join('events', 'list_of_participants.event', 'events.id')
+        //                 ->where('events.reminder', '<=', 'NOW()')
+        //                 ->update(['reminded' => 'true']);
        
-        foreach ($reminder as $reminders) {
-            //\Log::info($reminders['eventName']);
-            Mail::to($reminders->email)->send(new Reminder($reminders));
+        // foreach ($reminder as $reminders) {
+        //     //\Log::info($reminders['eventName']);
+        //     Mail::to($reminders->email)->send(new Reminder($reminders));
 
     }
 
