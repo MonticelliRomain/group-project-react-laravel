c620976432ad0a515279e4743ca7891de4594d07
matthieu <matthieu.jasselette@gmail.com>
2019-04-16 12:20:20 +0200

    corrected a few parameters on the iframe

--- resources/js/components/util/MediaPlayer.js
+++ resources/js/components/util/MediaPlayer.js
@@ -13,9 +13,9 @@ export default class MediaPlayer extends Component {
           width="560"
           height="315"
           src={this.props.package.image_url}
-          frameborder="0"
+          frameBorder="0"
           allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-          allowfullscreen>
+          allowFullScreen>
         </iframe>
         }
       </>
